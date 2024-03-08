9e62544232ff3bbb98e7082a8838f0d1dda758d0
Fabian_Martinez <f1a7b95@gmail.com>
2024-03-07 19:50:27 -0500

    JavaScript

---
+++ JavaScript/fundamentos.js
@@ -0,0 +1,57 @@
+// Este es un comentario de Java Script de una sola línea
+        /*
+        este es un
+        comentario de
+        varias líneas
+        */
+
+       /* Métodos de interacción */
+       // Salida
+        alert ("nothing is gonna work from this point!")
+    console.log("Mensaje a la consola")
+    document.write("Mensaje en interfaz de usuario")
+
+    /* VARIABLES */
+    // Declaración de variables
+   /*  Saludo1 = "Hola"
+
+    var saludo2 = "Hola 2"
+    let saludo3 = "hola 3"
+    const saludo4 = "Hola 4"
+
+    console.log(Saludo1)
+    console.log(saludo2)
+    console.log(saludo3)
+    console.log(saludo4) */
+
+    
+    // Ambito de las variables
+   /* let variableglobal = "soy variable global"
+    console.log(variableglobal)
+
+    {
+       variablelocal = "soy variable local"
+       console.log(variablelocal)
+    } */
+
+   /* let cambiaValor = 1;
+    const nocambiaValor = 2;
+
+    cambiaValor = "1";
+    //nocambiaValor = 2;
+
+    console.log(cambiaValor)
+    console.log(nocambiaValor) */
+
+    /* Modo estricto */
+    "use strict"
+
+    /* Tipos de datos primitivos */
+    // String
+    let miVariable = "este es un texto de ejemplo 1"
+    miVariable = 'este es un texto de "ejemplo 2"'
+    miVariable = "este es un texto de ejemplo 3"
+    miVariable = 10;
+        miVariable = 9.5;
+        miVariable = 123e5;
+    console.log(miVariable)
